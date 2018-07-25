40de422a950f7a4c12a6439fff2307f107b64228
lihan96 <fndnh@foxmail.com>
2018-07-25 19:41:21 +0800

    建立初始框架。

---
+++ mine_cleaner/Assets/Scripts/Scene/MainManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : System.Object {
+
+    private static MainManager instance;
+    
+    public ISceneController currentSceneController
+    {
+        get;
+        set;
+    }
+
+    public static MainManager GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new MainManager();
+        }
+
+        return instance;
+        
+    }
+
+}
